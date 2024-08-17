3ecfc1b72c6530bb8ce458da7bff6f95b3a3874b
Nagendra <nagendrababu.marasu@gmail.com>
2024-08-17 23:13:20 +0530

    Add. database properties to application.yml

---
+++ src/main/java/com/learnings/notes_app_graphql/NotesAppGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.learnings.notes_app_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnings/notes_app_graphql/NotesAppGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.notes_app_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
